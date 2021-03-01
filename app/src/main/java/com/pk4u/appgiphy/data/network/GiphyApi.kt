f03d7729568a50c62d21ad35dba8382ee7993395
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2021-03-01 21:46:40 +0200

    Create an interface for the API endpoint

---
+++ app/src/main/java/com/pk4u/appgiphy/data/network/GiphyApi.kt
@@ -0,0 +1,17 @@
+package com.pk4u.appgiphy.data.network
+
+import com.pk4u.appgiphy.model.TrendingResult
+import io.reactivex.Flowable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GiphyApi {
+
+    @GET("v1/gifs/trending")
+    fun getTrending(
+        @Query("api_key") apiKey: String,
+        @Query("limit") limit: String,
+        @Query("rating") rating: String
+    ): Flowable<TrendingResult>
+
+}
