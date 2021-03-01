f9ee87e83c14d692111ecc041de1085dc571b1e7
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2021-03-01 22:44:38 +0200

    Create the constant URL + KEY

---
+++ app/src/main/java/com/pk4u/appgiphy/Config.kt
@@ -0,0 +1,3 @@
+package com.pk4u.appgiphy
+
+const val KEY = "l3HmaFInnwUQPJ41dyVTi6HyHNm6BFfw"
---
+++ app/src/main/java/com/pk4u/appgiphy/internal/Constant.kt
@@ -0,0 +1,4 @@
+package com.pk4u.appgiphy.internal
+
+const val BASE_URL = "https://api.giphy.com/"
+const val DATABASE_NAME = "trending.db"
